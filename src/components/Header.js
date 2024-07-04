add0102c81077dedd7849a30601095ad99d967d1
Shitanshu <shitanshukumar639@gmail.com>
2024-07-04 15:46:24 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <p>
+        <span>Moves:</span>11
+      </p>
+      <p>
+        <span>Time:</span>00.39
+      </p>
+    </header>
+  );
+}
